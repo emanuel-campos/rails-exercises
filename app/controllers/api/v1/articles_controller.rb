b31a079efde7259429633b132de7f9b8cab59700
Emanuel Campos <emanuelcampos.contato@gmail.com>
2017-12-29 01:48:53 -0300

    implementar metodo para adicionar novo artigo

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,13 +3,29 @@ module Api
 		class ArticlesController < ApplicationController
 			# listar todos os artigos
 			def index
-				articles = Article.order('created_at DESC');
+				articles = Article.order('created_at DESC')
 				render json: {status: 'success', message: 'artigos carregados', data: articles}, status: :ok
 			end
+
 			#detalhe do artigo passando ID
 			def show
-				article = Article.find(params[:id]);
+				article = Article.find(params[:id])
 				render json: {status: 'success', message: 'artigo carregado', data: article}, status: :ok
+			end
+			
+			#adicionar novo artigo
+			def create
+				article = Article.new(article_params)
+				if article.save
+					render json: {status: 'success', message: 'artigo adicionado', data: article}, status: :ok
+				else
+					render json: {status: 'error', message: 'artigo não salvo', data: article.erros}, status: :unprocessable_entity
+				end
+			end
+			
+			#paramentros aceitos
+			def article_params
+				params.permit(:title, :body)
 			end
 		end
 	end
